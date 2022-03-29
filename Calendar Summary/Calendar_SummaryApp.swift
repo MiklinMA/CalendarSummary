86133be031289c6adf40281e6ba89e16bab23652
Mike Miklin <MiklinMA@gmail.com>
2022-03-29 23:26:57 +0500

    Initial Commit

---
+++ Calendar Summary/Calendar_SummaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calendar_SummaryApp.swift
+//  Calendar Summary
+//
+//  Created by Mike Miklin on 29.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct Calendar_SummaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calendar SummaryUITests/Calendar_SummaryUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Calendar_SummaryUITestsLaunchTests.swift
+//  Calendar SummaryUITests
+//
+//  Created by Mike Miklin on 29.03.2022.
+//
+
+import XCTest
+
+class Calendar_SummaryUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
